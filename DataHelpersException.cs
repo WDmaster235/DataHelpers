03d9b89516dcde1464c12bd9352dabd8d4b76a7c
WDmaster235 <131697799+WDmaster235@users.noreply.github.com>
2024-09-15 16:06:15 +0300

    Add files via upload

---
+++ DataHelpersException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataHelpers
+{
+    public class DataHelpersException : Exception
+    {
+        public DataHelpersException(string exception) : base(exception) { }
+        
+    }
+    public class EmptyException : DataHelpersException
+    {
+        public EmptyException(string exception) : base(exception) { }
+    }
+}
